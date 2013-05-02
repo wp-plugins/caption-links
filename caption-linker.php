eebf00c43a74ac56e83eb59b7404e9ada69ff160
weissmike <weissmike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-02 03:32:33 +0000

    First Commit
    git-svn-id: https://plugins.svn.wordpress.org/caption-links/trunk@706749 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ caption-linker.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Caption Linker
+Plugin URI: http://weissmike.com/caption-linker
+Description: Add "Insert Link" button to the WordPress caption field in the media window.
+Version: 1.0
+Author: Mike Weiss
+Author URI: http://weissmike.com
+License: GPL2
+*/
+/*  Copyright 2013  Mike Weiss  (email : mike@weissmike.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class caption_linker {
+    public function __construct()  
+    {
+        add_action( 'admin_enqueue_scripts', array($this, 'caption_linker_enqueue') );
+    }
+
+    public function caption_linker_enqueue($hook)  
+    {
+        wp_enqueue_script( 'caption_linker', plugins_url( 'caption-linker.js', __FILE__ ), array('jquery') );
+    }
+}
+
+$linker = new caption_linker();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Plugin Name ===
+Contributors: weissmike
+Donate link: http://example.com/
+Tags: media, caption, upload, file, link, html
+Requires at least: 3.4
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a very simple link button near the caption field of the media uploader.
+
+== Description ==
+
+Adds a new button to the media window and edit media screen that allow you to easily insert links into the caption field.
+
+== Installation ==
+
+1. Upload `caption-linker.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets 
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png` 
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
